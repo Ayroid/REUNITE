a04071dd3f9e76506b179fff5287c9d9aad53213
Ayroid <ayushsk0000@gmail.com>
2023-07-12 20:21:00 +0530

    Token verification removed

--- routers/claim.js
+++ routers/claim.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const claimRouter = express.Router();
-const {VERIFYTOKEN} = require('../authentication/jwt');
+// const {VERIFYTOKEN} = require('../authentication/jwt');
 
 const { claim } = require('../controllers/claim');
 
-claimRouter.post('/claim/:itemId',VERIFYTOKEN, claim);
+claimRouter.post('/claim/:itemId', claim);
 
 
 
